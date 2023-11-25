5d942053afed71928ef6dbfad0095d7d0b813195
Danpa_cho <danpa725@cau.ac.kr>
2023-11-25 22:48:39 +0900

    :wrench: setup: package environment

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import tsconfigPaths from "vite-tsconfig-paths"
+// eslint-disable-next-line import/no-unresolved
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+    root: process.cwd(),
+    plugins: [tsconfigPaths()],
+    test: {},
+    resolve: {},
+})
