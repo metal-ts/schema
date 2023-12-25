50dae5c6d4cefd13f31605ba0dc6d792394b236b
Danpa_cho <danpa725@cau.ac.kr>
2023-12-26 03:43:46 +0900

    :white_check_mark: test: update tuple test

--- packages/metal-type/src/__tests__/tuple.test.ts
+++ packages/metal-type/src/__tests__/tuple.test.ts
@@ -1,44 +1,48 @@
 import { describe, expect, it } from "vitest"
-import { t } from "../index"
-import { TupleSchema, tuple } from "../schema/tuple"
+import { t } from "../schema/t"
+import { TupleSchema } from "../schema/tuple"
 import { label } from "./utils/test.label"
 
 describe(label.unit("MetalType - TupleSchema"), () => {
     it(label.case("should create a tuple schema"), () => {
-        const schema = t.tuple([t.string(), t.number()])
+        const schema = t.tuple([t.string, t.number])
 
         expect(schema).toBeInstanceOf(TupleSchema)
         expect(schema.name).toBe("TUPLE")
-        expect(schema.tupleShape).toEqual([t.string(), t.number()])
+        expect(schema.shape).toEqual([t.string, t.number])
     })
 
     it(label.case("should create an optional tuple schema"), () => {
-        const schema = tuple([t.string(), t.number()]).optional()
+        const schema = t.tuple([t.string, t.number]).optional()
 
         expect(schema).toBeInstanceOf(TupleSchema)
         expect(schema.name).toBe("TUPLE | UNDEFINED")
-        expect(schema.tupleShape).toEqual([t.string(), t.number()])
+        expect(schema.shape).toEqual([t.string, t.number])
     })
 
     it(label.case("should parse tuple -> strict"), () => {
-        const ExtremeTuple = tuple([
-            t.string(),
-            t.number(),
-            t.boolean(),
-            t.symbol(),
-            t.undefined(),
-            t.null(),
-            t.any(),
-            t.unknown(),
+        const ExtremeTuple = t.tuple([
+            t.string,
+            t.number,
+            t.boolean,
+            t.symbol,
+            t.undefined,
+            t.null,
+            t.any,
+            t.unknown,
             t.literal("hello"),
             t.literal(1),
             t.literal(true),
             t.object({
-                hello: t.string(),
-                world: t.number(),
+                hello: t.string,
+                world: t.number,
                 deeply: t
                     .object({
-                        nested: t.string(),
+                        nested: t.string,
+                        a: t.object({
+                            b: t.string,
+                            c: t.boolean,
+                        }),
                     })
                     .optional(),
             }),
@@ -85,31 +89,31 @@ describe(label.unit("MetalType - TupleSchema"), () => {
     })
 
     it(label.case("should deep clone tupleShape -> zero side effect"), () => {
-        const schema = tuple([t.string(), t.number()])
-        const sameShape = schema.tupleShape
+        const schema = t.tuple([t.string, t.number])
+        const sameShape = schema.shape
 
         // @ts-expect-error readonly property, can't be modified by typescript
         // it can be modified by javascript, but it's not recommended
-        sameShape[0] = t.boolean()
-        expect(sameShape[0]).toEqual(t.boolean())
-        expect(schema.tupleShape[0]).toEqual(t.string())
+        sameShape[0] = t.boolean
+        expect(sameShape[0]).toEqual(t.boolean)
+        expect(schema.shape[0]).toEqual(t.string)
         const parsed = schema.parse(["not boolean", 1])
         expect(parsed).toEqual(["not boolean", 1])
     })
 
     it(label.case("should create a nullable tuple schema"), () => {
-        const schema = tuple([t.string(), t.number()]).nullable()
+        const schema = t.tuple([t.string, t.number]).nullable()
 
         expect(schema).toBeInstanceOf(TupleSchema)
         expect(schema.name).toBe("TUPLE | NULL")
-        expect(schema.tupleShape).toEqual([t.string(), t.number()])
+        expect(schema.shape).toEqual([t.string, t.number])
     })
 
     it(label.case("should create a nullish tuple schema"), () => {
-        const schema = tuple([t.string(), t.number()]).nullish()
+        const schema = t.tuple([t.string, t.number]).nullish()
 
         expect(schema).toBeInstanceOf(TupleSchema)
         expect(schema.name).toBe("TUPLE | NULL | UNDEFINED")
-        expect(schema.tupleShape).toEqual([t.string(), t.number()])
+        expect(schema.shape).toEqual([t.string, t.number])
     })
 })
