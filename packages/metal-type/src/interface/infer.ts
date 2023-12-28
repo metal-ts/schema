b0b35171a2f9ae32f9093efc3bb9250ec0bf6ae1
Danpa_cho <danpa725@cau.ac.kr>
2023-12-29 01:37:18 +0900

    :bug: fix: invalid inference type Infer

--- packages/metal-type/src/interface/infer.ts
+++ packages/metal-type/src/interface/infer.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { Schema, SchemaShape } from "../schema"
-import type { PRIMITIVES_UNIT_NAMES, WITH_TRANSFORM_MARK } from "./type"
+import type { Schema } from "../schema"
+import type { PRIMITIVE_SCHEMA_NAMES } from "./schema.names"
+import type { Prettify } from "./util"
 
 /**
  * @description get optional field from record
@@ -45,37 +46,27 @@ type GetRequiredField<Record> = {
 }
 type GetOptionalObject<T> = GetRequiredField<T> & GetOptionalField<T>
 
-type InferArray<T> = T extends Array<infer U> ? Array<U> : never
 /**
  * @description Get type of schema
  */
-export type Infer<T> = T extends Schema<infer Name, any, infer OutputType>
-    ? Name extends "OBJECT"
-        ? OutputType extends SchemaShape
-            ? never
-            : Infer<OutputType>
-        : Name extends PRIMITIVES_UNIT_NAMES
-          ? OutputType
-          : Name extends "ARRAY"
-            ? OutputType extends SchemaShape
-                ? never
-                : InferArray<Infer<OutputType>>
-            : Name extends WITH_TRANSFORM_MARK<string>
-              ? OutputType
-              : Infer<OutputType>
-    : T extends readonly [infer U, ...infer Rest]
-      ? Rest extends never[]
-          ? readonly [Infer<U>]
-          : readonly [Infer<U>, ...Infer<Rest>]
-      : T extends Record<string, any>
-        ? Prettify<
-              GetOptionalObject<{
-                  [Key in keyof T]: Infer<T[Key]>
-              }>
-          >
-        : T
-
-type Prettify<T> = {
-    [K in keyof T]: T[K]
-    // eslint-disable-next-line @typescript-eslint/ban-types
-} & {}
+export type Infer<T> = T extends Schema<infer Name, any, infer Output>
+    ? Name extends PRIMITIVE_SCHEMA_NAMES
+        ? Output
+        : Name extends "ARRAY"
+          ? Output extends Array<infer ArrayElement>
+              ? Array<Infer<ArrayElement>>
+              : Infer<Output>
+          : Name extends "TUPLE"
+            ? Output extends readonly [infer First, ...infer Rest]
+                ? readonly [Infer<First>, ...Infer<Rest>]
+                : Infer<Output>
+            : Name extends "OBJECT"
+              ? Prettify<
+                    GetOptionalObject<{
+                        [Key in keyof Output]: Infer<Output[Key]>
+                    }>
+                >
+              : Infer<Output>
+    : T extends readonly [infer First, ...infer Rest]
+      ? readonly [Infer<First>, ...Infer<Rest>]
+      : T
